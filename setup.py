ecb80944e49b07ac83e81dc7731a9dafd7579503
AbigailNkama <nkamaabigail1@mail.com>
2024-10-10 13:06:41 +0200

    updates added

---
+++ setup.py
@@ -0,0 +1,6 @@
+# setup.py
+from setuptools import setup
+
+setup(name='toto',
+      description="package description",
+      packages=["personal_assg_project"]) # You can have several packages, try it
